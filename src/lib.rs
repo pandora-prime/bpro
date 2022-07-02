5f7f393fb35520a574529801467fcd2aa42388bb
Dr. Maxim Orlovsky <orlovsky@pandoraprime.ch>
2022-07-03 01:33:18 +0200

    Move model from mycitadel-desktop

--- src/lib.rs
+++ src/lib.rs
@@ -9,11 +9,41 @@
 // a copy of the AGPL-3.0 License along with this software. If not, see
 // <https://www.gnu.org/licenses/agpl-3.0-standalone.html>.
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
+#[macro_use]
+extern crate amplify;
+#[macro_use]
+extern crate strict_encoding;
+#[cfg(feature = "serde")]
+extern crate serde_crate as serde;
+#[cfg(feature = "serde")]
+#[macro_use]
+extern crate serde_with;
+
+mod electrum;
+pub mod file;
+mod onchain;
+pub mod psbt;
+mod sign;
+mod taptree;
+mod template;
+mod types;
+mod wallet;
+
+pub use electrum::{ElectrumPreset, ElectrumSec, ElectrumServer};
+pub use file::FileDocument;
+pub use onchain::{
+    AddressSource, AddressSummary, AddressValue, HistoryEntry, OnchainStatus, OnchainTxid, Prevout,
+    TxidMeta, UtxoTxid,
+};
+pub use sign::XprivSigner;
+pub use taptree::ToTapTree;
+pub use template::{Requirement, WalletTemplate};
+pub use types::{
+    Error, HardwareDevice, HardwareList, OriginFormat, Ownership, Signer, SigsReq,
+    TimelockDuration, TimelockReq, TimelockedSigs,
+};
+
+pub use self::wallet::{
+    DerivationStandardExt, DerivationType, DescriptorError, SpendingCondition, Wallet,
+    WalletDescriptor, WalletEphemerals, WalletSettings, WalletState,
+};
