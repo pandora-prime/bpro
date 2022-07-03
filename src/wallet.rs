71985c496f4ded43655d8f89aa57e1f657f703d0
Dr Maxim Orlovsky <orlovsky@pandoraprime.ch>
2022-07-03 10:06:00 +0200

    Cargo fmt fixes

--- src/wallet.rs
+++ src/wallet.rs
@@ -1,9 +1,9 @@
-// MyCitadel desktop wallet: bitcoin & RGB wallet based on GTK framework.
+// Rust bitcoin wallet library for professional use.
 //
 // Written in 2022 by
 //     Dr. Maxim Orlovsky <orlovsky@pandoraprime.ch>
 //
-// Copyright (C) 2022 by Pandora Prime Sarl, Switzerland.
+// Copyright (C) 2022 by Pandora Prime SA, Switzerland.
 //
 // This software is distributed without any warranty. You should have received
 // a copy of the AGPL-3.0 License along with this software. If not, see
